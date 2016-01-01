2c63705ae93d139bdc4d336607aba4be2b60447f
Kurt D <kurtd5105@gmail.com>
2015-12-31 17:09:51 -0800

    Added metric measuring over an hour. Closes #1

--- logger.py
+++ logger.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 #from sensor_hat import SenseHat
 import sys
+import time
+from datetime import datetime
+from datetime import timedelta
 from itertools import product
 
 #Create a dictionary for the representation of the numbers in a pixel array
@@ -94,6 +97,18 @@ extraDigit = [
     [1, 0, 1, 1, 0, 1, 1, 1]
 ]
 
+
+class DummySenseHat():
+    def get_temperature(self):
+        return 20
+
+    def get_pressure(self):
+        return 1.0
+
+    def get_humidity(self):
+        return 50
+
+
 #Create an empty 8x8 array
 empty = [[0 for x in range(8)] for y in range(8)]
 
@@ -115,4 +130,40 @@ def generateNumberGroupings(numbers, groupingLength, screenDimensions, numberDim
 
 if __name__ == '__main__':
     #sense = SenseHat()
+    sense = DummySenseHat()
     groupings = generateNumberGroupings(numbers, 2, (5, 8), (5, 4))
+    now = datetime.now()
+    target = datetime.now()
+    metric = [0, [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
+    while True:
+        data = []
+
+        #From t 0 to 59 
+        for i in range(60):
+            start = datetime.now()
+            #print(start)
+            #Take measurements
+            data.append([str(start), sense.get_temperature(), sense.get_pressure(), sense.get_humidity()])
+
+            #Display measurements here
+
+            #Add a 60 second time delta from the start
+            target = timedelta(seconds = 60) - (datetime.now() - start)
+            time.sleep(target.total_seconds())
+
+        start = datetime.now()
+        metrics = [str(start)]
+        data.append([str(start), sense.get_temperature(), sense.get_pressure(), sense.get_humidity()])
+
+        #Calculate metrics here
+        metric = []
+        for i in range(1, 4):
+            metricData = [d[i] for d in data]
+            metric.append([sum(metricData) / len(metricData), min(metricData), max(metricData)])
+        metrics.append(metric)
+        #print(metrics)
+
+        #Log metrics and data here
+
+        target = timedelta(seconds = 60) - (datetime.now() - start)
+        time.sleep(target.total_seconds())
